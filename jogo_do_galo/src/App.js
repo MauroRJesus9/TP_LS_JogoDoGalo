a610520a56a0143800f65ca62fb3db5fc9f43fe8
Mauro Jesus <mauro.andre2000@gmail.com>
2023-04-26 11:06:16 +0100

    pedir nome do jogadores e jogo do galo bastante simples

--- jogo_do_galo/src/App.js
+++ jogo_do_galo/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tabuleiro from "./componentes/tabuleiro";
+import "./App.css";
 
 function App() {
+  const [jogador1, setJogador1] = useState("");
+  const [jogador2, setJogador2] = useState("");
+  const [gamestart, setGameStart] = useState(false);
+
+  const handleJogador1MudarNome = (event) => {
+    setJogador1(event.target.value);
+  };
+
+  const handleJogador2MudarNome = (event) => {
+    setJogador2(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //console.log("Nome jogador 1: ", jogador1);
+    //console.log("Nome jogador 2: ", jogador2);
+    setGameStart(true);
+    // do something with the player names, like pass them to a game component
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!gamestart ? (
+        <>
+          <h4>Bem vindo ao jogo do galo</h4>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome jogador 1:
+              <input
+                type="text"
+                value={jogador1}
+                onChange={handleJogador1MudarNome}
+              />
+            </label>
+            <br />
+            <label>
+              Nome jogador 2:
+              <input
+                type="text"
+                value={jogador2}
+                onChange={handleJogador2MudarNome}
+              />
+            </label>
+            <br />
+            <button type="submit">Começar jogo</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <Tabuleiro jogador1={jogador1} jogador2={jogador2} />
+        </>
+      )}
+    </>
   );
 }
 
---
+++ jogo_do_galo/src/componentes/tabuleiro.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function Board(props) {
+  function renderSquare(i) {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares, jogador1, jogador2) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a] === "X" ? jogador1 : jogador2;
+    }
+  }
+  return null;
+}
+
+function Tabuleiro(props) {
+  const { jogador1, jogador2 } = props;
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    const current = history.slice(0, stepNumber + 1);
+    const currentBoard = current[current.length - 1];
+    const squares = [...currentBoard.squares];
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([...current, { squares }]);
+    setStepNumber(current.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares, jogador1, jogador2);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? jogador1 : jogador2}`;
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={handleClick} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Tabuleiro;
