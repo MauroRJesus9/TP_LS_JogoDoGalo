3f43d16d100770f5f2fc8cf4f31c425db84be561
Pedro Paiva <96551783+pdrpaiva@users.noreply.github.com>
2023-05-18 10:56:56 +0100

    a

--- jogo_do_galo/src/componentes/control-bar/control-bar.component.jsx
+++ jogo_do_galo/src/componentes/control-bar/control-bar.component.jsx
@@ -5,16 +5,18 @@ export default function ControlBar(props){
     const {timerX, timerO, activeTimer, handleSair} = props;
 
     return (
-        <section id="control-bar">
-            <dl className={activeTimer ? "list-item active" : "list-item"} > {/*concatenar com a cor do jogador*/}
-                <dt>Jogador X:</dt>
-                <dd id="playerTimer">{timerX}</dd>
-            </dl>
-            <dl className={!activeTimer ? "list-item active" : "list-item"}> {/*concatenar com a cor do jogador*/}
-                <dt>Jogador O:</dt>
-                <dd id="playerTimer">{timerO}</dd>
-            </dl>
-            <button id="leaveBtn" onClick={handleSair}>SAIR</button>
-        </section>
+        <>
+            <section id="control-bar">
+                <dl className={activeTimer ? "list-item active" : "list-item"} > {/*concatenar com a cor do jogador*/}
+                    <dt>Jogador X:</dt>
+                    <dd id="playerTimer">{timerX}</dd>
+                </dl>
+                <dl className={!activeTimer ? "list-item active" : "list-item"}> {/*concatenar com a cor do jogador*/}
+                    <dt>Jogador O:</dt>
+                    <dd id="playerTimer">{timerO}</dd>
+                </dl>
+            </section>
+            <button onClick={handleSair}>SAIR</button>
+        </>
     );
 }
